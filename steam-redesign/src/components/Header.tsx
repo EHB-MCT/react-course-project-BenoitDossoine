905c0ca237db219489be61647ce3cf703a033253
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-04-05 18:15:31 +0200

    Adds Header component

---
+++ steam-redesign/src/components/Header.tsx
@@ -0,0 +1,11 @@
+function Header(){
+    return(
+        <header className="header">
+            <img src="" alt="" />
+            <a href="">Store</a>
+            <a href="">Library</a>
+        </header>
+    )
+}
+
+export default Header;
