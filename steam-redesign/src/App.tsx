8191e762163d17510bceecbfc7cbb6bf8ad28e33
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-04-05 19:18:40 +0200

    Adds title and subtitle of library

--- steam-redesign/src/App.tsx
+++ steam-redesign/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
+import Library from './components/Library';
+
 
 function App() {
   return(
-    <Header />
+    <>
+      <Header />
+      <Library />
+    </>
   );
 }
 
---
+++ steam-redesign/src/components/Library.tsx
@@ -0,0 +1,10 @@
+function Library(){
+    return(
+        <div className="library">
+            <h1 className="libraryTitle ">Library</h1>
+            <h2 className="librarySubtitle">Your games</h2>
+        </div>
+    );
+}
+
+export default Library;
