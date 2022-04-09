039f614bb2cf9d148998280cedd290b1c7b4292b
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-04-09 20:08:19 +0200

    Adds handleClick function and changes in dom hierarchy
    handleClick toggles the view of the extra game stats for each tile
    Added a div "libraryTileContainer" as a wrapper

--- steam-redesign/src/components/LibraryTile.tsx
+++ steam-redesign/src/components/LibraryTile.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { gameService } from "../services/GamesService";
 import { getRandomDate } from "../utils/randomDate";
+import GameStats from "./GameStats";
 
 function LibraryTile(props:any){
     const [isLoading, setIsLoading] = React.useState(true);
+    const [showStats, setShowStats] = React.useState(true);
     const [gameInfo,setGameInfo] = React.useState({});
 
     React.useEffect(()=>{
@@ -12,31 +14,38 @@ function LibraryTile(props:any){
             .then(()=>setIsLoading(false))
             .catch(error => console.log(error));
         
-        },[props.game.appid]);
+    },[props.game.appid]);
+
+    const handleClick = ()=>{
+        setShowStats(!showStats);
+    }
     
-        const renderTile = (gameInfo:any)=>{
-            console.log(props);
-            let lastPlayed = getRandomDate(new Date(2022,2,1), new Date());
-            return (
-                <>
-                    <img src={`${gameInfo.data.header_image}`} alt="" />
-                    <div className="libraryTileInfo">
-                        <h1>{gameInfo.data.name}</h1>
-                        <p>Total playtime: {`${props.game.playtime_forever}`} hours</p>
-                        <p>Last played: {lastPlayed.toLocaleDateString()}</p>
-                    </div>
-                    <div className="dropdownBtn">More stats</div>
-                </>
-                
-            )
-        }
+    const renderTile = (gameInfo:any)=>{
+        console.log(props);
+        let lastPlayed = getRandomDate(new Date(2022,2,1), new Date());
+        return (
+            <div className="libraryTile">
+                <img src={`${gameInfo.data.header_image}`} alt="" />
+                <div className="libraryTileInfo">
+                    <h1>{gameInfo.data.name}</h1>
+                    <p>Total playtime: {`${props.game.playtime_forever}`} hours</p>
+                    <p>Last played: {lastPlayed.toLocaleDateString()}</p>
+                </div>
+                <div className="dropdownBtn" onClick={handleClick}>More stats</div>
+            </div>
+            
+        )
+    }
     
     return(
-        <div className="libraryTile">
+        <div className="libraryTileContainer">
             {isLoading?
             <p>Loading...</p>:
-            renderTile(gameInfo)
-        }
+            renderTile(gameInfo)}
+            {showStats?
+            <GameStats/>:
+            null
+            }
         </div>
     );
 }
