da3a95c9254729552386c95de8d20973310ff45d
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-04-14 17:43:56 +0200

    Puts library title in caps

--- steam-redesign/src/components/Library.tsx
+++ steam-redesign/src/components/Library.tsx
@@ -3,7 +3,7 @@ import LibraryList from "./LibraryList";
 function Library(){
     return(
         <div className="library">
-            <h1 className="libraryTitle ">Library</h1>
+            <h1 className="libraryTitle ">LIBRARY</h1>
             <h2 className="librarySubtitle">Your games</h2>
             <LibraryList/>
         </div>
