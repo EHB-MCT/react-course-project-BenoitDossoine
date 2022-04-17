85f26ee71883528960d78a77597fe604a125f66f
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-04-17 14:35:25 +0200

    getLibraryGames update
    getLibraryGames now returns an object with general and user info

--- steam-redesign/src/services/GamesService.ts
+++ steam-redesign/src/services/GamesService.ts
@@ -1,14 +1,46 @@
+import UserGame from "../interfaces/UserGame";
+
 class GamesService{
     private userId = "76561198090377206"
 
     public async getLibraryGames(){
-        let response = await fetch(`https://steam-redesign.herokuapp.com/userGames/?id=${this.userId}`);
-        return response.json();
+        let tempGames = await fetch(`https://steam-redesign.herokuapp.com/userGames/?id=${this.userId}`)
+            .then(response => response.json())
+            .then(data => data.response.games);
+        
+        // let games = await tempGames.map(async (game:UserGame) =>{
+        //     let gameInfo = await this.getGameInfo(game.appid);
+            
+        //     let completeGame = {
+        //         img_icon_url:game.img_icon_url,
+        //         playtime_forever: game.playtime_forever,
+        //         ...gameInfo
+        //     };
+        //     return completeGame;
+        // })
+        
+        let gamePromise = await tempGames.map(async (game:UserGame) => {
+            let gameInfo = await this.getGameInfo(game.appid)
+            
+            let completeGame = {
+                appid: game.appid,
+                img_icon_url:game.img_icon_url,
+                playtime_forever: game.playtime_forever,
+                ...gameInfo
+            };
+            return completeGame
+
+        })
+        let games = Promise.all(gamePromise).then(games => games);
+        
+        return await games;
     }
 
     public async getGameInfo(gameId:number){
-        let response = await fetch(`https://steam-redesign.herokuapp.com/game/?id=${gameId}`);
-        return response.json();
+        let gameInfo = await fetch(`https://steam-redesign.herokuapp.com/game/?id=${gameId}`)
+            .then(response => response.json())
+            .then(data => data[gameId].data);
+        return gameInfo;
     }
 }
 
