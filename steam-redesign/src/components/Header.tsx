a5e531cf4811cd18d05e3b77fe6a5cdeb149384c
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-04-23 14:26:29 +0200

    Adds header with hover animation on links

--- steam-redesign/src/components/Header.tsx
+++ steam-redesign/src/components/Header.tsx
@@ -1,9 +1,19 @@
+import {FaSteam,FaRegHeart} from "react-icons/fa";
+import {IoCartOutline} from "react-icons/io5";
+
 function Header(){
     return(
         <header className="header">
-            <img src="" alt="" />
-            <a href="">Store</a>
-            <a href="">Library</a>
+            <div className="header-section header-left">
+                <div className="header-logo"><FaSteam/></div>
+                <a href="">Store</a>
+                <a href="">Library</a>
+            </div>
+            <div className="header-section header-right">
+                <p>Welcome back, <span>Benoît</span></p>
+                <div className="header-icon header-heart"><FaRegHeart/></div>
+                <div className="header-icon header-cart"><IoCartOutline/></div>
+            </div>
         </header>
     )
 }
