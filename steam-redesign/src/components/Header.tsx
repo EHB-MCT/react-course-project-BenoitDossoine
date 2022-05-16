43c5574ceabebdaa2fc5efc4541d80db8133608a
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-05-16 14:40:44 +0200

    Header changes

--- steam-redesign/src/components/Header.tsx
+++ steam-redesign/src/components/Header.tsx
@@ -1,18 +1,15 @@
-import {FaSteam,FaRegHeart} from "react-icons/fa";
-import {IoCartOutline} from "react-icons/io5";
+import {FaSteam} from "react-icons/fa";
 
 function Header(){
     return(
         <header className="header">
             <div className="header-section header-left">
                 <div className="header-logo"><FaSteam/></div>
-                <a href="">Store</a>
-                <a href="">Library</a>
+                <a href="https://steamredesignangular.web.app/">Store</a>
+                <a href="/">Library</a>
             </div>
             <div className="header-section header-right">
                 <p>Welcome back, <span>Benoît</span></p>
-                <div className="header-icon header-heart"><FaRegHeart/></div>
-                <div className="header-icon header-cart"><IoCartOutline/></div>
             </div>
         </header>
     )
